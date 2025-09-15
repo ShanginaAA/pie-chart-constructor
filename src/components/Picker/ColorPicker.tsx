ad1953404791d9f4fa754cbd14247e11852c57cd
Alexshan20 <shangina.1998@gmail.com>
2025-09-15 13:48:50 +0300

    update ColorPicker

--- src/components/Picker/ColorPicker.tsx
+++ src/components/Picker/ColorPicker.tsx
@@ -1,25 +1,54 @@
 import { colord } from 'colord';
-import { FC, useMemo, useState } from 'react';
+import { FC, useState } from 'react';
 import { RgbaStringColorPicker } from 'react-colorful';
-import { Grid, TextField, InputAdornment, styled, TextFieldProps } from '@mui/material';
+import {
+  Grid,
+  TextField,
+  InputAdornment,
+  styled,
+  TextFieldProps,
+  Box,
+  Typography,
+} from '@mui/material';
 
 const ColorPicker: FC = () => {
   const [color, setColor] = useState<string>('rgba(156, 84, 98, 0.71)');
   console.log(color);
   return (
-    <Grid>
+    <Grid className={'custom-layout'}>
       <RgbaStringColorPicker color={color} onChange={setColor} style={{ width: 'auto' }} />
+      <svg width={32} height={32} viewBox={`0 0 100 100`}>
+        <circle cx="50" cy="50" r="45" fill={color} />
+      </svg>
 
-      <Grid>
-        <Input
-          value={colord(color).toHex().substring(1)}
-          slotProps={{
-            input: {
-              disableUnderline: true,
-              startAdornment: <InputAdornment position="start">#</InputAdornment>,
-            },
-          }}
-        />
+      <Grid display={'flex'} alignItems={'center'} justifyContent={'space-between'}>
+        <Grid
+          display={'flex'}
+          flexDirection={'column'}
+          alignItems={'center'}
+          width={'322px'}
+          gap={1}>
+          <Input
+            value={colord(color).toHex().substring(1)}
+            slotProps={{
+              input: {
+                disableUnderline: true,
+                startAdornment: (
+                  <InputAdornment
+                    position="start"
+                    sx={{ '& .MuiTypography-root': { fontSize: 14 } }}>
+                    #
+                  </InputAdornment>
+                ),
+              },
+            }}
+          />
+          <Typography fontWeight={400} fontSize={14} lineHeight={'20px'} color="#99A1B7">
+            HEX
+          </Typography>
+        </Grid>
+
+        <Box component="img" sx={{ height: '20px', width: '20px' }} src="./images/arrow.png" />
       </Grid>
     </Grid>
   );
@@ -34,6 +63,7 @@ const Input = styled((props: TextFieldProps) => <TextField variant="standard" {.
       paddingTop: '16px',
     },
     '& .MuiInputBase-root': {
+      height: '28px',
       textDecoration: 'none',
       fontSize: 14,
       position: 'relative',
@@ -42,7 +72,7 @@ const Input = styled((props: TextFieldProps) => <TextField variant="standard" {.
       borderRadius: '5px',
       overflow: 'hidden',
       textOverflow: 'ellipsis',
-      padding: '6px 4px',
+      padding: '4px 6px',
       '&:hover': {
         borderColor: `#B2BAC2`,
       },
